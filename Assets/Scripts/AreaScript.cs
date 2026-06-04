ea1b51db104f2eb60a58d77f9065217a7e39bee5
Jamie Mitchell <jammiemitch@gmail.com>
2026-06-04 20:04:16 +0100

    Added AreaUI
    -UI icon at the bottom of the screen displaying where the player currently is

--- Assets/Scripts/AreaScript.cs
+++ Assets/Scripts/AreaScript.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using TMPro;
+using System;
 public class AreaScript : MonoBehaviour
 {
     public enum Areas { 
@@ -14,22 +16,28 @@ public class AreaScript : MonoBehaviour
 
     public Areas CurrentArea;
     public Item[] CurrentItemPool;
-
+  
     public Item[] FarmLand_ItemPool;
     public Item[] Ocean_ItemPool;
     public Item[] City_ItemPool;
     public Item[] Artic_ItemPool;
 
+
+
+    //All the stuff other than area defs
+    private TextMeshProUGUI AreaText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        AreaText = GameObject.Find("AreaUI").GetComponentInChildren<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        AreaText.text = Convert.ToString(CurrentArea);
 
 
         //Switches ItemPool Depending corrosponding with Area
