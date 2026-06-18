26194e3c836a48a7d57c6f1a755cda97ad20a8a2
Jamie Mitchell <jammiemitch@gmail.com>
2026-06-18 19:16:04 +0100

    BuildProfile

--- Assets/Scripts/AreaScript.cs
+++ Assets/Scripts/AreaScript.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using TMPro;
 using System;
-using UnityEditor.ShaderKeywordFilter;
 public class AreaScript : MonoBehaviour
 {
     public enum Areas { 
